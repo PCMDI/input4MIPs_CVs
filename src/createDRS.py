a062339bda542fc1653aab2d09be4dad5153e9fd
Paul J. Durack <me@pauldurack.com>
2024-07-18 15:16:37 -0700

    working version (#37)

--- src/createDRS.py
+++ src/createDRS.py
@@ -1,21 +1,28 @@
 #!/bin/env python
 
 """
- This script takes a path, evaluates for netcdf files creating output directories based on file metadata
+This script takes two paths, evaluates for netcdf files with required global
+attributes, creates output directories based on file metadata/global attributes
+and copies files to these locations
 
 To run conversion:
-$ durack1$ python createDRS.py $filePath
+$ durack1$ python createDRS.py $filePath $destPath
 
 """
 """2024
 PJD  9 Jul 2024 - started
+PJD 18 Jul 2024 - working for SOLARIS-HEPPA-CMIP-4-2 data
 """
 
 # %% imports
 import argparse
+import copy
 import datetime
 import glob
+import json
 import os
+import pdb
+import shutil
 import sys
 import xcdat as xc
 from urllib.request import urlopen
@@ -27,15 +34,20 @@ parser.add_argument(
     type=str,
     help="Provide a path where files exist",
 )
+parser.add_argument(
+    "destPath",
+    type=str,
+    help="Provide a path where new directories should be created",
+)
 argDict = parser.parse_args()
+# filePath
 filePath = argDict.filePath
-# test path
+# test filePath
 if os.path.exists(filePath):
     print("filePath:", filePath)
 else:
     print("Not valid filePath:", filePath)
     sys.exit()
-
 # test for valid files
 fileList = glob.glob(os.path.join(filePath, "*.nc"))
 if fileList:
@@ -44,9 +56,24 @@ else:
     print("No valid files, exiting")
     sys.exit()
 
+# destPath
+destPath = argDict.destPath
+# test destPath
+if os.path.exists(destPath):
+    print("destPath:", destPath)
+else:
+    print("Not valid destPath:", destPath)
+    sys.exit()
+
+# %% function defs
+
+
 # %% get registered info to check against
-target_url = "https://raw.githubusercontent.com/PCMDI/input4MIPs_CVs/main/README.md"
-txt = urlopen(target_url).read().decode("utf-8")
+target_url = "https://raw.githubusercontent.com/PCMDI/input4MIPs_CVs/main/CVs/input4MIPs_DRS.json"
+txt = urlopen(target_url)
+DRS = json.load(txt)
+DRSPath = DRS["directory_path_template"]
+print("DRSPath:", DRSPath)
 
 # %% interrogate each file for valid global attributes
 
@@ -60,26 +87,35 @@ attList = [
     "grid_label",
     "institution_id",
     "mip_era",
-    "nominal_resolution",  # extra
+    "nominal_resolution",
     "realm",
     "source",
     "source_id",
     "source_version",
     "target_mip",
     "variable_id",
-    "version",  # ESGF identifier
 ]
+#    "version",  # ESGF identifier
 
 drsList = [
-    ""
+    "activity_id",
+    "mip_era",
+    "target_mip",
+    "institution_id",
+    "source_id",
+    "realm",
+    "frequency",
+    "variable_id",
+    "grid_label",
 ]
+# version assigned by this script
 
 # match with file contents
 for cnt1, fPath in enumerate(fileList):
     print("processing:", fPath)
     fH = xc.open_dataset(fPath)
     attDic = fH.attrs
-    print(attDic)
+    # print(attDic)
     fH.close()
 
     # create checklist
@@ -94,11 +130,28 @@ for cnt1, fPath in enumerate(fileList):
         vars()[att] = attDic[att]
         print(att, ":", eval(att))
 
-# catch custom entries/fixes
-solhep42 = {}
-solhep42["nominal_resolution"] = "250 km"
-solhep42["source_version"] = "4.2"
-solhep42["version"] = datetime.datetime.strftime("%Y%m%d")
+    # catch custom entries/fixes
+    # solhep42 = {}
+    # solhep42["nominal_resolution"] = "250 km"
+    # solhep42["source_version"] = "4.2"
 
-
-# %% take attributes, create DRS
+    # %% take attributes, create DRS
+    drs = DRSPath
+    version = datetime.datetime.now().strftime("%Y%m%d")
+    print()
+    print("drs:", drs)
+    attList2 = copy.deepcopy(attList)
+    attList2.append("version")
+    for att in attList2:
+        attSwitch = "".join(["<", att, ">"])
+        print("switching:", attSwitch, eval(att))
+        drs = drs.replace(attSwitch, eval(att))
+    print("drs:", drs)
+    print("fPath:", fPath)
+    print("destPath:", destPath)
+    # test for existing directory and create
+    destFullPath = os.path.join(destPath, drs)
+    print("destFullPath:", destFullPath)
+    os.makedirs(destFullPath, exist_ok=True)
+    destFullPathAndFile = os.path.join(destPath, drs, fPath.split("/")[-1])
+    shutil.copy(fPath, destFullPathAndFile)
