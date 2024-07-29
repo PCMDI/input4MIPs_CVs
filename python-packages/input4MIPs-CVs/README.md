96daf477c770bf526e02e6217916dd78b40a0813
Zebedee Nicholls <zebedee.nicholls@climate-resource.com>
2024-07-29 11:48:21 +0200

    Add python package skeleton

---
+++ python-packages/input4MIPs-CVs/README.md
@@ -0,0 +1,5 @@
+# Input4MIPs-CVs
+
+Python tooling to help with the input4MIPs-CVs repository
+
+At the moment, intended for internal use only.
---
+++ python-packages/input4MIPs-CVs/src/input4MIPs_CVs/__init__.py
@@ -0,0 +1,9 @@
+"""
+Python tooling to help with the input4MIPs-CVs repository
+
+At the moment, intended for internal use only.
+"""
+
+import importlib.metadata
+
+__version__ = importlib.metadata.version("input4MIPs_CVs")
