492491be127022914b8cbfaad41c9ec510abb40b
Paul J. Durack <me@pauldurack.com>
2024-10-08 13:59:54 -0700

    updating createDRS.py to catch issues (#37)

--- scripts/createDRS.py
+++ scripts/createDRS.py
@@ -14,6 +14,7 @@ PJD  9 Jul 2024 - started
 PJD 18 Jul 2024 - working for SOLARIS-HEPPA-CMIP-4-2 data
 PJD 28 Aug 2024 - switched out logic, so DRS generation depends on drsList, rather
                   than broader list
+PJD  8 Oct 2024 - add back in check for tracking_id and creation_date format
 """
 
 # %% imports
@@ -23,6 +24,7 @@ import datetime
 import glob
 import json
 import os
+import re
 import shutil
 import sys
 import xcdat as xc
@@ -69,6 +71,32 @@ else:
 # %% function defs
 
 
+def checkCreationDate(creationDate):
+    """
+    Validate creation_date entry matches format that ESGF publisher expects
+    creation_date = 2023-05-12T13:46:40Z
+    """
+    creationDateFormat = re.compile(r"\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z")
+    if not creationDateFormat.fullmatch(creationDate):
+        print()
+        print("** creation_date:", creationDate, "format mismatch **")
+        print()
+
+
+def checkTrackingId(trackingId):
+    """
+    Validate tracking_id entry matches format ESGF publisher expects
+    tracking_id = hdl:21.14100/b52e19fb-de3f-301d-96f3-8b4b16ff8646
+    """
+    trackingIdFormat = re.compile(
+        r"hdl:21\.14100\/[0-9a-z]{8}-[0-9a-z]{4}-[0-9a-z]{4}-[0-9a-z]{4}-[0-9a-z]{12}"
+    )
+    if not trackingIdFormat.fullmatch(trackingId):
+        print()
+        print("** tracking_id:", trackingId, "format mismatch **")
+        print()
+
+
 # %% get registered info to check against
 target_url = "https://raw.githubusercontent.com/PCMDI/input4MIPs_CVs/main/CVs/input4MIPs_DRS.json"
 txt = urlopen(target_url)
@@ -78,14 +106,18 @@ print("DRSPath:", DRSPath)
 
 # %% interrogate each file for valid global attributes
 
-# create attribute list for searching
-attList = [
+# absolutely essential attribute list from CMIP6
+# https://docs.google.com/document/d/1pU9IiJvPJwRvIgVaSDdJ4O0Jeorv_2ekEtted34K9cA/edit#bookmark=id.xfvfxbc88ali
+absEssAttList = [
+    "Conventions",
     "activity_id",
     "contact",
+    "creation_date",
     "dataset_category",
     "frequency",
     "further_info_url",
     "grid_label",
+    "institution",
     "institution_id",
     "mip_era",
     "nominal_resolution",
@@ -94,8 +126,13 @@ attList = [
     "source_id",
     "source_version",
     "target_mip",
+    "title",
+    "tracking_id",
     "variable_id",
 ]
+
+# ESGF attributes required by publisher
+esgfAttList = ["creation_date", "tracking_id"]
 #    "version",  # ESGF identifier
 
 drsList = [
@@ -111,6 +148,9 @@ drsList = [
 ]
 # version assigned by this script
 
+# preallocate tracking_id list to catch duplicates
+trackingIdList = []
+
 # match with file contents
 for cnt1, fPath in enumerate(fileList):
     print("processing:", fPath)
@@ -123,11 +163,26 @@ for cnt1, fPath in enumerate(fileList):
     fileAttList = list(attDic.keys())
     print(fileAttList)
 
-    # process
-    for att in attList:
+    # process absolutely essential list
+    for att in absEssAttList:
         if att not in fileAttList:
-            print("**att:", att, "not included, skipping")
+            print("** att:", att, "not included, skipping **")
             continue
+        vars()[att] = attDic[att]
+        print(att, ":", eval(att))
+
+    # process ESGF publisher required
+    for att in esgfAttList:
+        if att not in fileAttList:
+            print("** att:", att, "not included, exiting **")
+            sys.exit()
+        # capture tracking_id
+        if att == "tracking_id":
+            trackingIdList.append(attDic[att])
+        # test trackingIdList for duplicates
+        if len(trackingIdList) != set(trackingIdList):
+            print("** att:", att, "duplicate found, exiting **")
+            sys.exit()
         vars()[att] = attDic[att]
         print(att, ":", eval(att))
 
