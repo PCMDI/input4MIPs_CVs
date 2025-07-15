ebb560b7eca4f308ca3a9e5c245c284d4866fbb1
Zebedee Nicholls <zebedee.nicholls@climate-resource.com>
2025-07-15 16:42:22 +0200

    Add scratch script

---
+++ scratch.py
@@ -0,0 +1,66 @@
+"""
+
+Notes for doing this via STAC API:
+https://hamedalemo.github.io/advanced-geo-python/lectures/earth_search_tutorial.html
+
+Really good example
+"""
+
+import globus_sdk
+import pandas as pd
+import tqdm.auto as tqdm
+
+pd.options.display.max_rows = None
+pd.options.display.max_columns = None
+
+esgf_uuid = "a8ef4320-9e5a-4793-837b-c45161ca1845"
+
+# # Hmm doesn't work - can't seem to get token
+# app = globus_sdk.UserApp(client_id=esgf_uuid)
+# print(f"{app.login_required()=}")
+#
+# transferer = globus_sdk.TransferClient(app=app)
+# for page in transferer.paginated.endpoint_search(
+#     query_params={"activity_id": "input4MIPs"}
+# ):
+#     for res in page:
+#         print("Endpoint ID: {}".format(res["id"]))
+#     breakpoint()
+
+
+searcher = globus_sdk.SearchClient()
+sr = searcher.search(esgf_uuid, "")
+res = []
+
+data_search = dict(
+    q="*",
+    filters=[
+        {
+            "type": "match_any",
+            "field_name": facet,
+            "values": values,
+        }
+        for facet, values in [
+            ("activity_id", ["input4MIPs"]),
+            ("mip_era", ["CMIP7"]),
+            ("type", ["Dataset"]),
+        ]
+    ],
+)
+search_res = searcher.post_search(esgf_uuid, data=data_search)
+scroll_res = searcher.paginated.scroll(esgf_uuid, data=data_search)
+for i, page in tqdm.tqdm(
+    enumerate(tqdm.tqdm(scroll_res, total=search_res.data["total"]))
+):
+    content = [e["content"] for v in page.data["gmeta"] for e in v["entries"]]
+    res.extend(content)
+
+res_df = pd.DataFrame(res)
+for c in res_df:
+    if res_df.loc[:, c].iloc[0] != res_df.loc[:, c].iloc[1]:
+        print(c)
+        print(res_df.loc[:, c].iloc[:2].tolist())
+
+
+# Can then use URLs etc. to actually download stuff
+# Can use pandas to avoid duplication etc.
