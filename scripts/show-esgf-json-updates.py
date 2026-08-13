6dab203d740467bef2b89f0fa712a8477c5233c2
Zebedee Nicholls <zebedee.nicholls@climate-resource.com>
2026-08-13 11:34:33 +1000

    Update links

--- scripts/show-esgf-json-updates.py
+++ scripts/show-esgf-json-updates.py
@@ -58,20 +58,21 @@ def main():
         # ):
         #     continue
 
+        base_node = "metagrid.esgf-west.org"
         if (
-            entry_latest["data_node"] == "esgf-node.ornl.gov"
+            entry_latest["data_node"] == base_node
             and entry_base["data_node"] == "eagle.alcf.anl.gov"
         ):
             continue
 
         if (
-            entry_latest["data_node"] == "esgf-node.ornl.gov"
+            entry_latest["data_node"] == base_node
             and entry_base["data_node"] == "aims3.llnl.gov"
         ):
             continue
 
         if (
-            entry_latest["data_node"] == "esgf-node.ornl.gov"
+            entry_latest["data_node"] == base_node
             and entry_base["data_node"] == "esgf-data.nersc.gov"
         ):
             continue
